dbdd622ae497172a486cdc0d72ae83b042827ab6
Fernando Souza <fernandovbsouza@gmail.com>
2018-06-26 13:37:30 -0300

    add function to check the type of the project

--- helpers.py
+++ helpers.py
@@ -3,6 +3,8 @@ import json
 import pyrebase
 import click
 import string
+import re
+
 from subprocess import run, PIPE
 from dynaconf import settings
 
@@ -25,15 +27,16 @@ def execute_bundle(command, base=''):
         path = os.path.abspath(os.path.join(root_dir,sub_dir, vhost_base))
 
         if os.path.isdir(path) and wp_cli_exists(path):
-            click.echo(f"Scanning {sub_dir} virtualhost now...")
-            p = run(command, cwd=path, stdout=PIPE)
-            if p.stdout:
-                click.echo('Done!')
-                vhost = sanitize_keys(sub_dir)
-                if base:
-                    response[vhost] = {base: json.loads(p.stdout.decode())}
-                else:
-                    response[vhost] = json.loads(p.stdout.decode())
+            if 'legba4' == type_of_application(path):
+                click.echo(f"Scanning {sub_dir} virtualhost now...")
+                p = run(command, cwd=path, stdout=PIPE)
+                if p.stdout:
+                    click.echo('Done!')
+                    vhost = sanitize_keys(sub_dir)
+                    if base:
+                        response[vhost] = {base: json.loads(p.stdout.decode())}
+                    else:
+                        response[vhost] = json.loads(p.stdout.decode())
 
     return response
 
@@ -64,3 +67,20 @@ def create_dataset(response):
 def sanitize_keys(string_key):
     letters = string.ascii_letters
     return ''.join([i if i in letters else '_' for i in string_key])
+
+def type_of_application(path):
+    p = run(['git', 'config', '--get', 'remote.origin.url'], cwd=path, stdout=PIPE, stderr=PIPE)
+
+    if p.stderr:
+        return False
+    
+    if p.stdout:
+        if 'legba4' in p.stdout:
+            return 'legba4'
+        if 'legba3' in p.stdout: 
+            return 'legba3'
+        else:
+            repo = re.split("[/.-_:", p.stdout)
+            return repo[-2] if len(repo) > 1 else False 
+            
+    return False
